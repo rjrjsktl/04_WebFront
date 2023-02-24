c5b5b9601496c0e9a12d879689f13e8e5e5d6cc7
YUN <rjrjsktl@gmail.com>
2023-02-24 21:38:32 +0900

    230224 - 계산기 및 이벤트 배움

---
+++ js/05_간이계산기.js
@@ -0,0 +1,19 @@
+const num1 = document.getElementById("num1");
+const num2 = document.getElementById("num2");
+const result = document.getElementById("result");
+
+function plus() {
+    result.innerText = Number(num1.value) + Number(num2.value)
+}
+function minus() {
+    result.innerText = Number(num1.value) - Number(num2.value)
+}
+function muiti() {
+    result.innerText = Number(num1.value) * Number(num2.value)
+}
+function div() {
+    result.innerText = Number(num1.value) / Number(num2.value)
+}
+function mod() {
+    result.innerText = Number(num1.value) % Number(num2.value)
+}
